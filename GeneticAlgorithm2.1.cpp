63f162eee4ba94cd45c3da0b23fbad0217b8d3c9
Christopher Krauss <christopherjosekrauss@gmail.com>
2024-06-19 18:01:55 -0300

    Inseridoa função que gera os dados de comparação da função otimizada

--- GeneticAlgorithm2.1.cpp
+++ GeneticAlgorithm2.1.cpp
@@ -5,8 +5,9 @@
 int main()
 {
 	srand(time(NULL));
-	int nCromosomos = 4, nPopulation = 500, numInterations = 100;
+	int nCromosomos = 4, nPopulation = 500, numInterations = 1000;
 	float mutationRate, selectionPorcentage;
+	double FuncResposta[21];
 
 	float Population[500][5];
 	// A ultima casa, vai ser apenas para armazenar puntuações de fitness
@@ -20,7 +21,20 @@ int main()
 		}
 	}
 
-	
+	// Genera os dados para comparação da equação resposta
+	int u = 0;
+	for (float x = -1; x < 1.1; x += 0.1)
+	{
+		printf("[%.2f] \n", x);
+
+		FuncResposta[u] = -0.3 + 0.1 * x - 0.5 * x * x + 0.4 * x * x * x;
+		u++;
+	}
+
+	/*for (int i = 0; i < 20; i++)
+	{
+		printf("FuncResposta[%d] = [%.2lf] \n", i, FuncResposta[i]);
+	}*/
 
 
 
